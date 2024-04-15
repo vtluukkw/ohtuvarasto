0acc2eb8ed6d99eb937b0303e2821e27c39ac2c9
vtluukkw <valtteri.luukkala@gmail.com>
2024-04-15 18:10:35 +0300

    tehtava 10

--- src/varasto.py
+++ src/varasto.py
@@ -39,7 +39,7 @@ class Varasto:
 
         self.saldo = self.saldo - maara
 
-        return False
+        return maara
 
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
